bda86e2b243171d0fa5ac2a3f9b68cbbc7965ef6
Thomas <t.o.c.cheng@student.tudelft.nl>
2025-01-09 16:35:06 +0100

    Initial commit

--- src/FlatMat.jl
+++ src/FlatMat.jl
@@ -1,5 +1,7 @@
 module FlatMat
 
-# Write your package code here.
+include("FMat.jl")
+
+export FMat
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,39 @@
 using FlatMat
+using Random.Random
 using Test
 using Aqua
 using JET
 
 @testset "FlatMat.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(FlatMat)
+  # @testset "Code quality (Aqua.jl)" begin
+  #   Aqua.test_all(FlatMat)
+  # end
+  # @testset "Code linting (JET.jl)" begin
+  #   JET.test_package(FlatMat; target_defined_modules=true)
+  # end
+
+  elements = Vector{Vector{Int}}()
+  for i in 1:7
+    nrows = rand(UInt8) + 1
+    for _ in 1:nrows
+      push!(elements, abs.(rand(Int16, i)))
     end
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(FlatMat; target_defined_modules = true)
+  end
+  fm = FMat(elements)
+
+  function array_is_same()
+    is_same::Bool = true
+    for (i, val) in enumerate(elements)
+      is_same = is_same && fm[i] == val
     end
-    # Write your tests here.
+    return is_same
+  end
+
+  @test array_is_same()
+  @test fm[1:end] == FMat(elements[1:end])
+  @test fm[2:5] == FMat(elements[2:5])
+  @test fm[end:end] == FMat(elements[end:end])
+
+  # @test FMat([])
+  # @test FMat([[], []])
 end
